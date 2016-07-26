5e2d309f3eed8c5ac1f3bcca3bd478b3d7627960
spartan99 <f.fulgeri@gmail.com>
2016-07-26 08:37:24 +0200

    servelt modified

--- src/com/auleSVGWT/server/AndroidImageServlet.java
+++ src/com/auleSVGWT/server/AndroidImageServlet.java
@@ -147,6 +147,7 @@ public class AndroidImageServlet extends HttpServlet {
                 }
                 else{
                     ArrayList<RoomPeopleDTO> roomPeopleDTOs;
+                    ArrayList<OccupyDTO> occupyDTOs;
                     ArrayList<String> room;
                     String build;
                     String floor;
@@ -156,18 +157,21 @@ public class AndroidImageServlet extends HttpServlet {
                     floor = name.substring(name.lastIndexOf("-") + 1);
 
                     room = listaAulePianoNewVersion(name);
-                    roomPeopleDTOs = getRoomsPeople(build, floor);
+                    //roomPeopleDTOs = getRoomsPeople(build, floor);
+                    occupyDTOs = getOccupy(build,floor);
 
                     //System.out.println(build +" ......"+floor);
 
 
                     if("2".equals(mod)){
-
-                        modalityTwo(room,roomPeopleDTOs,doc,out,transcoder);
+                        modalityTwoNew(room, occupyDTOs, doc, out, transcoder);
+                        //modalityTwo(room,roomPeopleDTOs,doc,out,transcoder);
                     }
                     else if("3".equals(mod)){
 
-                        modalityThree(room,roomPeopleDTOs,doc,out,transcoder);
+                        modalityThreeNew(room, occupyDTOs, doc, out, transcoder);
+
+                        //modalityThree(room,roomPeopleDTOs,doc,out,transcoder);
 
                     }
 
@@ -230,6 +234,7 @@ public class AndroidImageServlet extends HttpServlet {
         }
 
     }
+
     public void modalityTwo(ArrayList<String> strings, ArrayList<RoomPeopleDTO> roomPeopleDTOs,Document doc,OutputStream out,PNGTranscoder transcoder){
 
         int sum;
@@ -375,7 +380,7 @@ public class AndroidImageServlet extends HttpServlet {
     }
 
 
-    public ArrayList<OccupyDTO> getOccupy(String building, String floorSt, String numberSt){
+    public ArrayList<OccupyDTO> getOccupy(String building, String floorSt){
 
         int floor = Integer.parseInt(floorSt);
 
@@ -537,5 +542,154 @@ public class AndroidImageServlet extends HttpServlet {
 
         return new RoomDTO(room.getId(), room.getNumber(), room.getFloor(),
                 createBuildingDTO(room.getBuilding()), room.getMaxPeople(), room.getDimension(),room.getRoomCode(),room.getMaintenance(),room.getSocket());
+    }
+
+
+    public void modalityTwoNew(ArrayList<String> strings, ArrayList<OccupyDTO> occupyDTOs,Document doc,OutputStream out,PNGTranscoder transcoder){
+
+        int sum;
+        int dim;
+
+
+        for (String room : strings) {
+            sum = 0;
+            dim = 0;
+            Boolean first = true;
+            for (OccupyDTO occupyDTO : occupyDTOs) {
+                String s = "";
+                s += occupyDTO.getRoom().getBuilding().getName() + "-" + Integer.toString(occupyDTO.getRoom().getFloor()) +
+                        "-" + Integer.toString(occupyDTO.getRoom().getNumber());
+
+                System.out.println(""+room +"  ===  "+ s);
+
+                if (room.equals(s)) {
+                    System.out.println("\n sono uguali");
+                    sum += occupyDTO.getPerson().getRole().getSqm();
+
+                    if(occupyDTO.getRoom().getDimension()!=0 && first) {
+                        System.out.println("\n dim=" + occupyDTO.getRoom().getDimension());
+                        dim = occupyDTO.getRoom().getDimension();
+                        first = false;
+                    }
+
+
+
+
+                }
+
+            }
+            if (dim == 0 || sum == 0) {
+                String style = doc.getElementById(room).getAttribute("style");
+                style = style.replaceFirst("fill:none", "fill:grey");
+                doc.getElementById(room).setAttribute("style", style);
+
+            } else if (sum == dim /*&& dim != 0*/) {
+                String style = doc.getElementById(room).getAttribute("style");
+                style = style.replaceFirst("fill:none", "fill:green");
+                doc.getElementById(room).setAttribute("style", style);
+            } else if (sum > dim /*&& dim != 0*/) {
+                //Integer value = new Integer ((1-(sum/Max))*200);
+                //Integer value = new Integer (((double)dim/sum)*200);
+                Double value1 = ((double) dim / sum) * 200;
+                Integer value = value1.intValue();
+
+                //varia da 0 a 200
+                String str = "#FF";
+                if (Integer.toHexString(value).length() <= 1) {
+                    str += "0" + Integer.toHexString(value) + "0" + Integer.toHexString(value);
+                } else {
+                    str += Integer.toHexString(value) + Integer.toHexString(value);
+                }
+                //Window.alert(str);
+                String style = doc.getElementById(room).getAttribute("style");
+                style = style.replaceFirst("fill:none", "fill:"+str);
+                doc.getElementById(room).setAttribute("style", style);
+            } else {
+                if (sum < dim /*&& dim != 0*/) {
+                    String str = "#";
+                    //Integer value = new Integer (((sum*200)/dim));
+
+                    Double value1 = ((double) sum / dim) * 200;
+                    Integer value = value1.intValue();
+
+                    //varia da 0 a 200
+
+                    if (Integer.toHexString(value).length() <= 1) {
+                        str += "0" + Integer.toHexString(value) + "0" + Integer.toHexString(value);
+                    } else {
+                        str += Integer.toHexString(value) + Integer.toHexString(value);
+                    }
+                    str += "FF";
+                    String style = doc.getElementById(room).getAttribute("style");
+                    style = style.replaceFirst("fill:none", "fill:" + str);
+                    doc.getElementById(room).setAttribute("style", style);
+                }
+            }
+
+
+        }
+
+        try{
+
+            //System.out.println("" + (new File("C:\\Users\\Utente\\IdeaProject\\esperimenti_vari\\src\\fac-0.svg.svg")).toURI().toString());
+            TranscoderInput input = new TranscoderInput(doc);
+            //FileOutputStream ostream = new FileOutputStream("C:\\Users\\Utente\\IdeaProject\\esperimenti_vari\\src\\");
+            TranscoderOutput output = new TranscoderOutput(out);
+            System.out.println("vediamo cosa passa    " + input.getURI());
+            transcoder.transcode(input, output);
+            out.flush();
+            out.close();
+
+        }catch(TranscoderException | IOException c){
+            c.printStackTrace();
+        }
+
+
+    }
+
+
+
+    public void modalityThreeNew(ArrayList<String> strings, ArrayList<OccupyDTO> occupyDTOs,Document doc,OutputStream out,PNGTranscoder transcoder){
+
+        for (String room : strings) {
+            for (OccupyDTO occupyDTO : occupyDTOs) {
+                String s = "";
+                s += occupyDTO.getRoom().getBuilding().getName() + "-" + Integer.toString(occupyDTO.getRoom().getFloor()) +
+                        "-" + Integer.toString(occupyDTO.getRoom().getNumber());
+                if (room.equals(s)) {
+                    if(occupyDTO.getPerson().getEndWork() != null){
+                            if(occupyDTO.getPerson().getEndWork().before(new Date(new java.util.Date().getTime()))){
+                                String style = doc.getElementById(room).getAttribute("style");
+                                style = style.replaceFirst("fill:none", "fill:red");
+                                doc.getElementById(room).setAttribute("style", style);
+
+                            }
+                    }
+
+
+
+
+                }
+
+            }
+
+
+        }
+
+        try{
+
+            //System.out.println("" + (new File("C:\\Users\\Utente\\IdeaProject\\esperimenti_vari\\src\\fac-0.svg.svg")).toURI().toString());
+            TranscoderInput input = new TranscoderInput(doc);
+            //FileOutputStream ostream = new FileOutputStream("C:\\Users\\Utente\\IdeaProject\\esperimenti_vari\\src\\");
+            TranscoderOutput output = new TranscoderOutput(out);
+            System.out.println("vediamo cosa passa    " + input.getURI());
+            transcoder.transcode(input, output);
+            out.flush();
+            out.close();
+
+        }catch(TranscoderException | IOException c){
+            c.printStackTrace();
+        }
+
     }
 }
